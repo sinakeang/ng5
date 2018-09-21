e79fd09199c5eca5f3e14b9fccd74fef6098f7b6
Sina Keang <sina.keang@innovapost.com>
2018-09-21 09:59:28 -0400

    finish remove item using animation

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -13,6 +13,12 @@ import { trigger,style,transition,animate,keyframes,query,stagger } from '@angul
           style({opacity: 0, transform: 'translateY(-75%)', offset: 0}),
           style({opacity: .5, transform: 'translateY(35px)', offset: .3}),
           style({opacity: 1, transform: 'translateY(0)', offset: 1}),
+        ]))]), {optional: true}),
+        query(':leave', stagger('300ms', [
+          animate('.6s ease-in', keyframes([
+          style({opacity: 1, transform: 'translateY(0)', offset: 0}),
+          style({opacity: .5, transform: 'translateY(35px)', offset: .3}),
+          style({opacity: 0, transform: 'translateY(-75%)', offset: 1}),
         ]))]), {optional: true})
       ])
     ])
@@ -36,4 +42,7 @@ export class HomeComponent implements OnInit {
     this.itemCount = this.goals.length;
   }
 
+  removeItem(i) {
+    this.goals.splice(i, 1);
+  }
 }
